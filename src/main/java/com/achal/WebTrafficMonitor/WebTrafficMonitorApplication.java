4c246e93f884a71a1dcae39f50e12a63e63f0372
Achal Sharma <44108410+Achal-Sharma@users.noreply.github.com>
2023-07-30 01:19:16 +0100

    Initial commit

---
+++ src/main/java/com/achal/WebTrafficMonitor/WebTrafficMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.achal.WebTrafficMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTrafficMonitorApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WebTrafficMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/achal/WebTrafficMonitor/WebTrafficMonitorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.achal.WebTrafficMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrafficMonitorApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
